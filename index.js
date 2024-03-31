fe6dddad059b29e669817ebeed48c3c53436184d
Rupayan Dirghangi <send2rupayan2002@gmail.com>
2024-03-31 20:22:28 +0530

    A new feature of image uploading is updated

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
+import multer from "multer";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -10,12 +11,24 @@ const port = 3000;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+//Storage and filename settings
+const storage = multer.diskStorage({
+  destination: "public/images/",
+  filename: (req, file, cb)=>{
+    cb(null, file.originalname);
+  }
+});
+
+const upload = multer({
+  storage: storage
+});
 
 mongoose.connect("mongodb://localhost:27017/userDB");
 
 const userSchema = {
   name: String,
   email: String,
+  image: String,
 };
 
 const User = mongoose.model("User", userSchema);
@@ -41,30 +54,44 @@ app.get("/edit_user/:id", async (req, res)=>{
           { 
             id: userId,
             name: userFind.name,
-            email: userFind.email 
+            email: userFind.email,
+            image: userFind.image
           });
         } catch (err) {
           console.log(err);
         }
 });
 
-app.post("/submit", async (req, res)=>{
+app.post("/submit", upload.single("image"), async (req, res)=>{
     const name = req.body.name;
     const email = req.body.email;
+    const image = req.file.filename;
     const newUser=new User({
         name: name,
-        email: email
+        email: email,
+        image: image
     });
     newUser.save();
     res.redirect("/");
 });
 
-app.post("/edit/:id", async (req, res)=>{
+app.post("/edit/:id", upload.single("image"), async (req, res)=>{
     const userId = req.params.id;
     const newName = req.body.name;
     const newEmail = req.body.email;
+    let newImage="";
+    if(req.file){
+      newImage = req.file.filename;
+    }else{
+      try {
+        const userFind = await User.findOne({_id: userId});
+        newImage = userFind.image;
+      } catch (err) {
+        console.log(err);
+      }
+    }
         try {
-          const userFind = await User.updateOne({_id: userId}, {$set: {name: newName, email: newEmail}});
+          const userFind = await User.updateOne({_id: userId}, {$set: {name: newName, email: newEmail, image: newImage}});
           console.log(userFind);
           res.redirect("/");
         } catch (err) {
